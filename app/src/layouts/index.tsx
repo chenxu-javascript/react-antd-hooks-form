04c3f6732cf9563591c8b103b7b3f03f16646f29
新年歌声 <843313654@qq.com>
2020-03-07 11:16:29 +0800

    all

--- app/src/layouts/index.tsx
+++ app/src/layouts/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import styles from './index.css';
+import styles from './index.less';
 // import 'antd/dist/antd.css';
 
 const BasicLayout: React.FC = props => {
   return (
-    <div className={styles.normal}>
+    <div >
       <h1 className={styles.title}>Yay! Welcome to umi!</h1>
       {props.children}
     </div>
